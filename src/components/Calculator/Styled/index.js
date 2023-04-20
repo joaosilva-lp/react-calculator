6215a233510c377d385e79d3aa707eaebfa8406b
João Silva <99473819+joaosilva-lp@users.noreply.github.com>
2023-04-20 16:53:10 +0100

    first commit

---
+++ src/components/Calculator/Styled/index.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    width: 400px;
+    margin: 40px auto;
+    grid-template-columns: repeat(4,100px);
+    grid-template-rows: minmax(120px, auto) repeat(5, 100px);
+    box-shadow: 2px 2px 10px #333;
+    border-radius: 10px;
+`
+
+export const Screen = styled.div`
+    grid-column: 1/-1;
+    background-color: rgba(60, 64, 67, 0.75);
+    diplay: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    padding: 10px;
+    word-wrap: break-word;
+    word-break: break-all;
+    text-align: right;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+`
+
+export const Previous = styled.div`
+    
+`
---
+++ src/components/Calculator/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Container, Screen} from "./Styled";
+
+export default function Calculator(){
+    return(
+        <Container> 
+            <Screen></Screen>
+        </Container>
+
+    )
+}
